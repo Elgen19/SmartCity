47bd8c9ac24f06f4c904f831fed9b6047d193eb3
Elgen19 <elgenprestosa@gmail.com>
2024-09-08 10:39:45 +0800

    added weather update on Dashboard activity

--- app/src/main/java/com/elgenium/smartcity/DashboardActivity.kt
+++ app/src/main/java/com/elgenium/smartcity/DashboardActivity.kt
@@ -1,23 +1,43 @@
 package com.elgenium.smartcity
 
+import android.Manifest
 import android.app.ActivityOptions
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.elgenium.smartcity.databinding.ActivityDashboardBinding
 import com.elgenium.smartcity.helpers.NavigationBarColorCustomizerHelper
+import com.elgenium.smartcity.models.WeatherResponse
+import com.elgenium.smartcity.network.OpenWeatherApiService
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.Calendar
+import java.util.Locale
 
 class DashboardActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDashboardBinding
     private lateinit var auth: FirebaseAuth
     private lateinit var database: DatabaseReference
+    private lateinit var apiService: OpenWeatherApiService
+    private val locationRequestCode = 1001
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var geocoder: Geocoder
+    private val apiKey = BuildConfig.OPEN_WEATHER_API
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +46,19 @@ class DashboardActivity : AppCompatActivity() {
 
         // sets the color of the navigation bar making it more personalized
         NavigationBarColorCustomizerHelper.setNavigationBarColor(this, R.color.secondary_color)
+
+        // Initialize Retrofit
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        apiService = retrofit.create(OpenWeatherApiService::class.java)
+
+        // Initialize location services
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        geocoder = Geocoder(this, Locale.getDefault())
+
+
 
         auth = FirebaseAuth.getInstance()
         database = FirebaseDatabase.getInstance().getReference("Users")
@@ -41,10 +74,80 @@ class DashboardActivity : AppCompatActivity() {
             finish()
         }
 
+        fetchWeather()
         updateGreeting()
         loadProfileImage()
         setupListeners()
     }
+
+
+    private fun fetchWeather() {
+        Log.d("DashboardActivity", "Fetching weather")
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    Log.d("DashboardActivity", "Location retrieved: Lat ${location.latitude}, Long ${location.longitude}")
+                    val latitude = location.latitude
+                    val longitude = location.longitude
+                    val addressList = geocoder.getFromLocation(latitude, longitude, 1)
+                    val cityName = addressList?.firstOrNull()?.locality ?: "Unknown"
+
+                    Log.d("DashboardActivity", "City name: $cityName")
+
+                    apiService.getWeather(cityName, apiKey).enqueue(object : Callback<WeatherResponse> {
+                        override fun onResponse(call: Call<WeatherResponse>, response: Response<WeatherResponse>) {
+                            if (response.isSuccessful) {
+                                val weatherResponse = response.body()
+                                weatherResponse?.let {
+                                    val weatherDescription = it.weather.firstOrNull()?.description ?: "Unknown"
+                                    val temperature = it.main.temp
+                                    val heatIndex = it.main.feels_like
+                                    val iconCode = it.weather.firstOrNull()?.icon ?: "01d"
+                                    val iconUrl = "https://openweathermap.org/img/wn/$iconCode.png"
+
+                                    Log.d("DashboardActivity", "Weather description: $weatherDescription")
+                                    Log.d("DashboardActivity", "Temperature: $temperature, Heat index: $heatIndex")
+                                    Log.d("DashboardActivity", "Icon URL: $iconUrl")
+
+                                    binding.weatherStatusText.text = weatherDescription.replaceFirstChar { char ->
+                                        if (char.isLowerCase()) char.titlecase(Locale.ROOT) else char.toString()
+                                    }
+                                    binding.temperatureText.text = getString(R.string.temperature_format, temperature)
+                                    binding.heatIndexValue.text = getString(R.string.temperature_format, heatIndex)
+                                    binding.cityNameText.text = cityName
+
+                                    // Load the weather icon into the ImageView
+                                    Glide.with(this@DashboardActivity)
+                                        .load(iconUrl)
+                                        .placeholder(R.drawable.cloud) // Optional placeholder
+                                        .into(binding.weatherIcon)
+                                }
+                            } else {
+                                Log.e("DashboardActivity", "Failed to get weather data: ${response.message()}")
+                                Toast.makeText(this@DashboardActivity, "Failed to get weather data", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+                        override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
+                            Log.e("DashboardActivity", "Error fetching weather data", t)
+                            Toast.makeText(this@DashboardActivity, "Error: ${t.message}", Toast.LENGTH_SHORT).show()
+                        }
+                    })
+                } else {
+                    Log.e("DashboardActivity", "Unable to retrieve location")
+                    Toast.makeText(this, "Unable to retrieve location", Toast.LENGTH_SHORT).show()
+                }
+            }
+        } else {
+            Log.e("DashboardActivity", "Location permission not granted")
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), locationRequestCode)
+        }
+    }
+
+
+
+
+
 
     private fun setupListeners() {
         binding.profileImage.setOnClickListener {
---
+++ app/src/main/java/com/elgenium/smartcity/models/WeatherData.kt
@@ -0,0 +1,19 @@
+package com.elgenium.smartcity.models
+
+
+data class WeatherResponse(
+    val weather: List<Weather>,
+    val main: Main,
+    val name: String
+)
+
+data class Weather(
+    val description: String,
+    val icon: String
+)
+
+data class Main(
+    val temp: Float,
+    val feels_like: Float
+)
+
