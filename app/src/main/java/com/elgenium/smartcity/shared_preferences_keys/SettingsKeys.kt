04f4cc00494861216db39090d3feba70556fe791
Elgen19 <elgenprestosa@gmail.com>
2024-10-13 19:52:55 +0800

    revert back to Open weather api for weather updates, added settings toggle for enable/disable event and context recommendation carousels at dashboard activity to help minimize api calls when this activity is called

---
+++ app/src/main/java/com/elgenium/smartcity/shared_preferences_keys/SettingsKeys.kt
@@ -0,0 +1,11 @@
+package com.elgenium.smartcity.shared_preferences_keys
+
+object SettingsKeys {
+    const val PREFS_NAME = "user_settings"
+    const val KEY_PUSH_NOTIFICATIONS = "push_notifications"
+    const val KEY_TRAFFIC_UPDATES = "traffic_updates"
+    const val KEY_EVENTS_NOTIFICATIONS = "events_notifications"
+    const val KEY_CONTEXT_RECOMMENDER = "context_recommender"
+    const val KEY_EVENT_RECOMMENDER = "event_recommender"
+
+}
